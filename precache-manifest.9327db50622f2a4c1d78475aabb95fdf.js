d451893a149fa9bc3180624bd4277052c79a8e86
Derek W <41767875+dwilli77@users.noreply.github.com>
2019-01-26 20:24:43 -0500

    Updates

---
+++ precache-manifest.9327db50622f2a4c1d78475aabb95fdf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "461a71bdab39c8cac5ed",
+    "url": "/clicky-game/static/js/main.461a71bd.chunk.js"
+  },
+  {
+    "revision": "b05f58ae460c5f47cea3",
+    "url": "/clicky-game/static/js/1.b05f58ae.chunk.js"
+  },
+  {
+    "revision": "461a71bdab39c8cac5ed",
+    "url": "/clicky-game/static/css/main.f341b56c.chunk.css"
+  },
+  {
+    "revision": "ae58cbcab8d679ae72aa132b071bfd51",
+    "url": "/clicky-game/index.html"
+  }
+];
